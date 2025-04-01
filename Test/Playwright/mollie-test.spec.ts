9759b31b20aefe63d0019d562f9ccedfc8fbf63c
Jisse Reitsma <jisse@yireo.com>
2025-04-01 16:49:47 +0200

    WIP

--- Test/Playwright/mollie-test.spec.ts
+++ Test/Playwright/mollie-test.spec.ts
@@ -1,45 +1,48 @@
-import FieldManager from "./helpers/field";
-import getCheckoutConfig from "./helpers/checkout-config";
-import {test, expect} from './fixtures/cart-page';
+const {Field, FieldManager} = require(process.cwd() + '/helpers/field');
+const saveCheckoutConfig = require(process.cwd() + '/helpers/checkout-config');
+const {test, expect} = require(process.cwd() + '/fixtures/checkout-page');
 
 test.describe.configure({mode: 'serial'});
 
 test.describe('Yireo_LokiCheckoutMollie test', () => {
     test('should allow me to go to the checkout', async ({checkoutPage, request}) => {
-        await getCheckoutConfig(request, [
-            'Yireo_LokiCheckoutMollie',
-            'Mollie_Payment',
-            'Yireo_LokiCheckoutHyvaSkinny',
-            'Yireo_DisableCsp',
-        ], {
-            'payment/mollie_general/enabled': 1,
-            'payment/mollie_general/type': 'test',
-            'payment/mollie_general/enable_magento_vault': 0,
-            'payment/mollie_general/default_selected_method': 'mollie_methods_ideal',
-            'yireo_loki_checkout/general/theme': 'onestep'
-        });
+
+        await saveCheckoutConfig(request, {
+            modules: [
+                'Yireo_LokiCheckoutMollie',
+                'Mollie_Payment',
+                'Yireo_DisableCsp',
+            ],
+            config: {
+                'payment/mollie_general/enabled': 1,
+                'payment/mollie_general/type': 'test',
+                'payment/mollie_general/enable_magento_vault': 0,
+                'payment/mollie_general/default_selected_method': 'mollie_methods_ideal',
+                'yireo_loki_checkout/general/theme': 'onestep'
+            }
+            });
 
         await checkoutPage.reload();
 
-        const fieldManager = new FieldManager(checkoutPage);
-        fieldManager.fillAll([
-            {id: 'email', group: 'customer', value: 'jane@example.com'},
-            {id: 'firstname', group: 'shipping', value: 'Jane'},
-            {id: 'lastname', group: 'shipping', value: 'Doe'},
-            {id: 'company', group: 'shipping', value: 'Yireo'},
-            {id: 'city', group: 'shipping', value: 'Baarn'},
-            {id: 'street0', group: 'shipping', value: 'Amalialaan'},
-            {id: 'street1', group: 'shipping', value: '126'},
-            {id: 'street2', group: 'shipping', value: 'D'},
-            {id: 'country-id', group: 'shipping', value: 'NL'},
-            {id: 'region', group: 'shipping', value: 'Utrecht'},
-            {id: 'phone', group: 'shipping', value: '0123456789'},
-            {id: 'fax', group: 'shipping', value: '0123456789'},
-            {id: 'vat-id', group: 'shipping', value: 'NL1234123412'},
-            {id: 'shipping-as-billing', group: 'billing', value: '1'},
+        const fieldManager = new FieldManager(checkoutPage, []);
+        fieldManager.fill([
+            ['customer.email', 'jane@example.com'],
+            ['shipping.firstname', 'Jane'],
+            ['shipping.lastname', 'Doe'],
+            ['shipping.company', 'Yireo'],
+            ['shipping.city', 'Baarn'],
+            ['shipping.street0', 'Amalialaan'],
+            ['shipping.street1', '126'],
+            ['shipping.street2', 'D'],
+            ['shipping.country-id', 'NL'],
+            ['shipping.region', 'Utrecht'],
+            ['shipping.phone', '0123456789'],
+            ['shipping.fax', '0123456789'],
+            ['shipping.vat-id', 'NL1234123412'],
+            ['billing.shipping-as-billing', '1']
         ]);
 
-        const paymentMethodField = fieldManager.getLocatorByHtmlId('payment-mollie_methods_ideal');
-        fieldManager.fill(paymentMethodField, true, 'radiobox');
+        const paymentMethodField = fieldManager.getField('payment-mollie_methods_ideal');
+        paymentMethodField.fill(true);
     });
 });
