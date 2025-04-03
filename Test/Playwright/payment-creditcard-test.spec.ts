5dea628a2248b8e286ef48e6c1a8fe8e9bdcf455
Jisse Reitsma <jisse@yireo.com>
2025-04-03 11:46:12 +0200

    Add creditcard test with components

--- Test/Playwright/payment-creditcard-test.spec.ts
+++ Test/Playwright/payment-creditcard-test.spec.ts
@@ -5,12 +5,13 @@ const {test} = require(process.cwd() + '/fixtures/checkout-page');
 import {MolliePortal} from './helpers/mollie-objects';
 import mollieConfig from './config/config';
 
-test.describe('creditcard payment test', () => {
+test.describe('creditcard payment without components test', () => {
     test('should allow me to go to the checkout', async ({page, context}) => {
         await saveCheckoutConfig(context, {
             ...mollieConfig,
             config: {
                 'payment/mollie_methods_creditcard/active': 1,
+                'payment/mollie_methods_creditcard/use_components': 0,
             }
         });
 
@@ -24,7 +25,39 @@ test.describe('creditcard payment test', () => {
 
         const molliePortal = new MolliePortal(page);
         await molliePortal.expectIssuerPage();
-
-        // @todo: Add tests for components and for vault
     });
 });
+
+
+test.describe('creditcard payment with components test', () => {
+    test('should allow me to go to the checkout', async ({page, context}) => {
+        await saveCheckoutConfig(context, {
+            ...mollieConfig,
+            config: {
+                'payment/mollie_methods_creditcard/active': 1,
+                'payment/mollie_methods_creditcard/use_components': 1,
+            }
+        });
+
+        await page.goto('/checkout');
+
+        const paymentMethod = new PaymentMethod(page, 'mollie_methods_creditcard');
+        await paymentMethod.select();
+
+        const idPrefix = '#loki-checkout-payment-payment-methods-mollie-methods-creditcard-form-';
+
+        await page.locator(idPrefix + 'card-holder iframe').contentFrame().locator('id=cardHolder').fill('J. B. Reitsma');
+        await page.locator(idPrefix + 'card-number iframe').contentFrame().locator('id=cardNumber').fill('378282246310005');
+        await page.locator(idPrefix + 'expiry-date iframe').contentFrame().locator('id=expiryDate').fill('03/30');
+        await page.locator(idPrefix + 'verification-code iframe').contentFrame().locator('id=verificationCode').fill('1111');
+
+        await page.waitForTimeout(1000); // Give it a second
+
+        const placeOrderButton = new PlaceOrderButton(page);
+        await placeOrderButton.click();
+
+        const molliePortal = new MolliePortal(page);
+        await molliePortal.expectIssuerPage();
+    });
+});
+
