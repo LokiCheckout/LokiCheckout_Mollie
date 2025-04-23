c5830db733b6dba02e395cf570c3179d066de67c
Jisse Reitsma <jisse@yireo.com>
2025-04-23 15:04:12 +0200

    Add helper comment in developer mode

--- Payment/Icon/IconResolver.php
+++ Payment/Icon/IconResolver.php
@@ -2,6 +2,7 @@
 
 namespace Yireo\LokiCheckoutMollie\Payment\Icon;
 
+use Magento\Framework\App\State;
 use Magento\Framework\Module\Manager as ModuleManager;
 use Mollie\Payment\Helper\General as GeneralHelper;
 use Yireo\LokiCheckout\Payment\Icon\IconResolverContext;
@@ -12,6 +13,7 @@ class IconResolver implements IconResolverInterface
     public function __construct(
         private ModuleManager $moduleManager,
         private GeneralHelper $mollieGeneralHelper,
+        private State $appState
     ) {
     }
 
@@ -26,19 +28,27 @@ class IconResolver implements IconResolverInterface
             return false;
         }
 
+        if ($paymentMethodCode === 'mollie_methods_creditcard_vault') {
+            $paymentMethodCode = 'mollie_methods_creditcard';
+        }
+
         if (!preg_match('/^mollie_methods_(.*)$/', $paymentMethodCode, $match)) {
             return false;
         }
 
-        if ($paymentMethodCode === 'mollie_methods_creditcard_vault') {
-            $paymentMethodCode = 'mollie_methods_creditcard';
-        }
+        //Mollie_Payment/images/creditcard-issuers/mastercard.svg
 
         $iconFilePath = $iconResolverContext->getIconPath(
             'Mollie_Payment',
             'view/frontend/web/images/methods/'.$match[1].'.svg'
         );
 
-        return $iconResolverContext->getIconOutput($iconFilePath, 'svg');
+        $iconOutput = $iconResolverContext->getIconOutput($iconFilePath, 'svg');
+
+        if ($this->appState->getMode() === State::MODE_DEVELOPER) {
+            $iconOutput .= '<!-- '.$iconFilePath.' -->';
+        }
+
+        return $iconOutput;
     }
 }
