bf1786425c434b3db4f2f731c042766cbde50837
Jisse Reitsma <jisse@yireo.com>
2025-05-26 11:26:07 +0200

    Increase timeouts

--- Test/Playwright/helpers/mollie-objects.ts
+++ Test/Playwright/helpers/mollie-objects.ts
@@ -9,13 +9,13 @@ export class MolliePortal {
     }
 
     async expectTestPaymentPage() {
-        await expect(this.page).toHaveURL(/www.mollie.com\/checkout/, {timeout: 5000});
+        await expect(this.page).toHaveURL(/www.mollie.com\/checkout/, {timeout: 10000});
 
         const body = await this.page.locator('body');
         await expect(body).toHaveText(/Note: this is a testmode payment/);    }
 
     async expectIssuerPage() {
-        await expect(this.page).toHaveURL(/www.mollie.com\/checkout/, {timeout: 5000});
+        await expect(this.page).toHaveURL(/www.mollie.com\/checkout/, {timeout: 10000});
 
         const body = await this.page.locator('body');
         await expect(body).toHaveText(/Note: this is a testmode payment/);
